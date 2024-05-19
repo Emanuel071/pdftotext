88bf473403b9f7df270787019822de8fc6301d06
Emanuel Calderon <emanuelcalderon071@gmail.com>
2024-05-19 16:03:15 -0400

    update toe script

--- pdfimagetotext.py
+++ pdfimagetotext.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path
 from pytesseract import image_to_string
 from PIL import Image
+from docx import Document
 
 def convert_pdf_to_img(pdf_file):
     return convert_from_path(pdf_file)
@@ -16,6 +17,25 @@ def get_text_from_any_pdf(pdf_file):
         final_text += convert_image_to_text(img)
     return final_text
    
+def string_var_to_word_doc(var_string):
+    # Create a new Document
+    doc = Document()
 
-path_to_pdf = '/Users/eacalder/Documents/Github/pdftotext/tests/declaration.pdf'
-print(get_text_from_any_pdf(path_to_pdf))
+    # Add a paragraph with the string
+    doc.add_paragraph(var_string)
+
+    # Save the document
+    doc.save('tests/my_document.docx')
+
+    print("Document saved successfully.")
+
+
+##MAIN##
+path_to_pdf = '/Users/eacalder/Documents/Github/pdftotext/tests/RulesnRegulations.pdf'
+
+stinrg_variable = get_text_from_any_pdf(path_to_pdf)
+print(stinrg_variable)
+
+string_var_to_word_doc(stinrg_variable)
+
+print("Job successful.")
